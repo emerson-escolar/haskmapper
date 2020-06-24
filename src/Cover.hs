fe3ed9a6512334ecc0ae4e9fdd8ba1f76bde66ab
Emerson Escolar <38313244+emerson-escolar@users.noreply.github.com>
2020-06-24 11:20:15 +0900

    define RectBound

--- src/Cover.hs
+++ src/Cover.hs
@@ -6,14 +6,15 @@ import qualified Numeric.LinearAlgebra as LA
 import qualified Numeric.LinearAlgebra.Data as LAD
 
 
-bounds :: RowData -> [(Double, Double)]
+bounds :: RowData -> RectBound
 bounds a =
   let cols = LAD.toColumns a
-  in  zip (map LAD.minElement cols) (map LAD.maxElement cols)
+  in  (LAD.vector (map LAD.minElement cols),
+       LAD.vector (map LAD.maxElement cols))
 
-uniformFences :: (Integral a, Enum b, Fractional b) =>
+uniformFences1D :: (Integral a, Enum b, Fractional b) =>
   a -> b -> (b, b) -> ([b], [b])
-uniformFences n p (x_min, x_max) =
+uniformFences1D n p (x_min, x_max) =
   let dn = fromIntegral n
       leg = (x_max-x_min) / (dn - p * (dn-1))
       stride = (1 - p) * leg
@@ -21,11 +22,12 @@ uniformFences n p (x_min, x_max) =
   in (map lowerFence [0..dn-1],
       map ((+leg).lowerFence) [0..dn-2] ++ [x_max])
 
-
 genHyperCubes :: (Integral a) =>
-  a -> Double -> [(Double,Double)] -> [(Point, Point)]
+  a -> Double -> RectBound -> [RectBound]
 genHyperCubes n p bds =
-  let (lbs, ubs) = unzip $ map (uniformFences n p) bds
+  let (lowerBd, upperBd) = bds
+      bds1D = zip (LAD.toList lowerBd) (LAD.toList upperBd)
+      (lbs, ubs) = unzip $ map (uniformFences1D n p) bds1D
       lbPoints = map LAD.fromList (sequence lbs)
       ubPoints = map LAD.fromList (sequence ubs)
   in zip lbPoints ubPoints
--- src/Types.hs
+++ src/Types.hs
@@ -1,7 +1,12 @@
 module Types where
 
 import qualified Numeric.LinearAlgebra.Data as LAD
+import qualified Data.Set as DS
 
 type RowData = LAD.Matrix Double
 
 type Point = LAD.Vector Double
+
+type RectBound = (Point, Point)
+
+type Cluster = DS.Set Point
